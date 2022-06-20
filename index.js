7b49d7864b3403c4a71b6e7f454812c774bbae36
Clara Pons <clarapons@macbook-air-de-clara.home>
2022-06-20 12:28:04 +0200

    exercices JS

---
+++ index.js
@@ -0,0 +1,41 @@
+// 01 - Hello World
+//console.log("Hello World")
+
+// 02 - String
+//const sentence = "My name is Clara"
+//console.log(sentence)
+
+// 03 - Concaténation
+//const name = "Clara"
+//console.log(`Nice to meet you ${name}`)
+
+// 04 - String Length
+//const sentence = "I'm a new text !"
+//console.log(sentence.length)
+
+// 05 - Replace
+//const food = "croissant is meh"
+//console.log(food.replace("meh","so good"))
+
+// 06 - Up and Down
+//const basic = "This is Cool"
+//console.log(basic)
+
+//const basicUp = basic.toUpperCase()
+//console.log(basicUp)
+
+//const basicDown = basic.toLowerCase()
+//console.log(basicDown)
+
+// 07 - Split
+//const word = "banana"
+//const letters = word.split("")
+//console.log(letters)
+
+//08 - Template
+//const age = 27
+//const template = `I'm ${age} years old`
+//console.log(template)
+
+// BONUS
+
